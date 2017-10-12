a472ab369fb9edd3f6cbb3b1200e09d242ae036c
Stephen Hyde-Donohue <schd001@comcast.net>
2017-10-12 11:47:02 -0700

    Added some small stuff to the parser

--- SurlyParser.java
+++ SurlyParser.java
@@ -5,10 +5,23 @@
   Fall 2017
   File parser for the SURLY DB
 */
+
+import java.util.Scanner;
+import java.io.*;
 public class SurlyParser {
 
-  public void parse (String file) {
+  public void parse (String filename) {
+    File file = new File(filename);
+    try {
+      Scanner scan = new Scanner(file);
+      int count = 0;
+      while(scan.hasNextLine()) {
 
+      }
+    }
+    catch(FileNotFoundException exception) {
+      System.out.println("The file " + file.getPath() + " doesn't exist.");
+    }
   }
 
 }
