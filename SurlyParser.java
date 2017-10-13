3e623e31cc240a115d956120eac3186b40adeec7
Stephen Hyde-Donohue <schd001@comcast.net>
2017-10-13 15:12:28 -0700

    Played around with parser still need to implement command classes

--- SurlyParser.java
+++ SurlyParser.java
@@ -8,14 +8,38 @@
 
 import java.util.Scanner;
 import java.io.*;
+
 public class SurlyParser {
 
   public void parse (String filename) {
     File file = new File(filename);
     try {
       Scanner scan = new Scanner(file);
-      int count = 0;
+      String command;
       while(scan.hasNextLine()) {
+        command = scan.next();
+
+        /* 
+          Simple not easily extendable version. Going to implement an abstract command
+          class and have the parser to iterate through commands find a match and then run.
+        */
+
+        if ("RELATION".equals(command)) {
+          //handle insert commands
+          //System.out.println("RELATION COMMAND");
+        }
+        else if ("INSERT".equals(command)) {
+          //handle insert commands
+          //System.out.println("INSERT COMMAND");
+        }
+        else if ("PRINT".equals(command)) {
+          //handle insert commands
+          //System.out.println("PRINT COMMAND");
+        }
+        else {
+          System.out.println("SURLY doesn't understand the command \'" + command + '\'');
+        }
+        scan.nextLine();
 
       }
     }
