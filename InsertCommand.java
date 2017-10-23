ea5e11332a296215c8fab823174e0edd345eddbe
richarz <richarz@wwu.edu>
2017-10-23 14:43:34 -0700

    Update InsertCommand.java

--- InsertCommand.java
+++ InsertCommand.java
@@ -11,7 +11,27 @@ public class InsertCommand extends BaseCommand {
   private String name = "INSERT";
 
   public void run(String params) {
+    String[] tokens = params.trim().split("\\s\\(|\\)|,\\s|;|\\s");
+    String relationName = tokens[0];
 
+    LinkedList<Relation> currDatabase = SurlyDatabase.getRelations();
+    int databaseLen = currDatabase.indexOf(currDatabase.getLast())
+    int iteratorVal = 0;
+    String currRelationName;
+    Tuple currRow;
+
+    while (iteratorVal < databaseLen) {
+      currRelationName = currDatabase.indexOf(iteratorVal).getRelName();
+      if (currRelationName = relationName) {
+
+        currRow = currDatabase.indexOf(iteratorVal).getLast();
+        Relation.insertTuple();
+        for (int x = 0; x < tokens.length; x++) {
+          currRow.insertAttrib(tokens[x]);
+        }
+        iteratorVal = databaseLen;
+      } else iteratorVal++;
+    }
   }
 
   public String getName() {
