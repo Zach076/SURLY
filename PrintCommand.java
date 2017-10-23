110d76ea896e8199d15256cef22f56591228db13
richarz <richarz@wwu.edu>
2017-10-23 14:44:49 -0700

    Update PrintCommand.java

--- PrintCommand.java
+++ PrintCommand.java
@@ -10,7 +10,21 @@ import java.util.Scanner;
 public class PrintCommand extends BaseCommand {
   private String name = "PRINT";
   public void run(String params) {
+    String[] tokens = params.trim().split("\\s\\(|\\)|,\\s|;|\\s");
+    String relationName = tokens[0];
 
+    LinkedList<Relation> currDatabase = SurlyDatabase.getRelations();
+    int databaseLen = currDatabase.indexOf(currDatabase.getLast())
+    int iteratorVal = 0;
+    Relation currRelation;
+
+    while (iteratorVal < databaseLen) {
+      currRelationName = currDatabase.indexOf(iteratorVal).getRelName();
+      if (currRelationName = relationName) {
+        currRelation = currDatabase.indexOf(iteratorVal);
+        currRelation.print()
+      }
+    }
   }
   public String getName() {
     return name;
