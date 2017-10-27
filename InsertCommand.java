cca026fe00c3267cd3a47d6ed29be8403218273a
Stephen Hyde-Donohue <hydedos.wwu.edu>
2017-10-27 11:48:27 -0700

    Added some error handling

--- InsertCommand.java
+++ InsertCommand.java
@@ -12,8 +12,9 @@ public class InsertCommand extends BaseCommand {
   private String name = "INSERT";
 
   public void run(String params) {
-    String[] tokens  = params.trim().split("\\s|;");
-    quoteHandler(tokens);
+
+    String[] tokens = quoteHandler(params.trim().split("\\s|;"));
+
 
     String relationName = tokens[0];
 
@@ -22,13 +23,18 @@ public class InsertCommand extends BaseCommand {
     int iteratorVal = 0;
     Relation currRelation;
     Tuple currRow;
+    boolean relationFound = false;
 
     while (iteratorVal < databaseLen) {
       currRelation = currDatabase.get(iteratorVal);
       if (currRelation.getName().equals(relationName)) {
-
+        relationFound = true;
         currRelation.insertTuple();
         currRow = currRelation.getTuples().getLast();
+        if ((tokens.length - 1) != currRelation.getDomain().size()) {
+          System.out.println("ERRMSG4");
+          System.exit(1);
+        }
         for (int x = 1; x < tokens.length; x++) {
           Attribute newattribute = new Attribute(tokens[x]);
           currRow.getAttributes().add(newattribute);
@@ -36,16 +42,20 @@ public class InsertCommand extends BaseCommand {
         iteratorVal = databaseLen;
       } else iteratorVal++;
     }
+    if (!relationFound) {
+      System.out.println("Invalid relation name \'" + relationName + "\'");
+    }
   }
 
   public String getName() {
     return name;
   }
 
-  private void quoteHandler (String[] tokens) {
+  private String[] quoteHandler (String[] tokens) {
     boolean foundQuote = false;
     int startQuote = 0;
     int endQuote = 0;
+
 
     for (int j = 1; j < tokens.length; j++) {
       if (tokens[j].charAt(0) == '\'' && foundQuote == false) {
@@ -66,6 +76,12 @@ public class InsertCommand extends BaseCommand {
         }
       }
       tokens[startQuote] = tokens[startQuote].replaceAll("\'","");
+
     }
+    String[] newTokens = new String[tokens.length - (endQuote - startQuote)];
+    for (int i = 0; i < newTokens.length; i++) {
+      newTokens[i] = tokens[i];
+    }
+    return newTokens;
   }
 }
