8e98be0c96b8bd6969d4981bc3b7d7e1176b5ce8
Stephen Hyde-Donohue <Stephen@Stephens-MacBook-Pro.local>
2017-11-22 15:55:53 -0800

    Fixed issues with insert from surly 1

--- InsertCommand.java
+++ InsertCommand.java
@@ -22,18 +22,20 @@ public class InsertCommand extends BaseCommand {
 
     if (index >= 0) {
       Relation currRelation = relations.get(index);
-      currRelation.insertTuple();
-      Tuple currRow = currRelation.getTuples().getLast();
       if ((tokens.length - 1) != currRelation.getDomain().size()) {
         // Incorrect amount of attributes
         System.out.println("ERRMSG4");
         System.exit(1);
       }
+      currRelation.insertTuple();
+      Tuple currRow = currRelation.getTuples().getLast();
       for (int x = 1; x < tokens.length; x++) {
-        if (tokens[x].length() > currRelation.getDomain().get(x - 1).getMaxLen() ) {
-          System.out.println(tokens[x] + " exceeds maxlength of " + currRelation.getDomain().get(x - 1).getMaxLen());
+        if (checkDatatype(tokens[x], currRelation, x)) {
+          String value = checkMaxlen(tokens[x], currRelation, x);
+          currRow.getAttributes().add(new Attribute(value));
         } else {
-          currRow.getAttributes().add(new Attribute(tokens[x]));
+          currRelation.getTuples().removeLast();
+          System.out.println("ERRMSG2");
         }
       }
     } else {
@@ -75,5 +77,49 @@ public class InsertCommand extends BaseCommand {
       newTokens[i] = tokens[i];
     }
     return newTokens;
+  }
+
+  private boolean checkDatatype(String data, Relation relation, int index) {
+    String datatype = relation.getDomain().get(index - 1).getDatatype();
+    if (datatype.equals("NUM")) {
+      for (int i = 0; i < data.length(); i++) {
+        if (!Character.isDigit(data.charAt(i))) {
+          System.out.println(data.charAt(i) + " in " + data + " is not a \'NUM\'");
+          return false;
+        }
+      }
+    } else if (datatype.equals("CHAR")) {
+      return true;
+    } else {
+      System.out.println("Invalid stored datatype in a relation while inputting");
+      return false;
+    }
+    return true;
+  }
+  private String checkMaxlen(String data, Relation relation, int index) {
+    int maxLength = relation.getDomain().get(index - 1).getMaxLen();
+    String datatype = relation.getDomain().get(index - 1).getDatatype();
+
+    if (data.length() >  maxLength) {
+      int startIndex = 0;
+      int endIndex = 0;
+      if (datatype.equals("NUM")) {
+        //truncate to the left
+        startIndex = data.length() - maxLength;
+        endIndex = data.length();
+        System.out.println(startIndex + ", " + endIndex);
+        System.out.println("ERRMSG3");
+      } else {
+        //truncate to the left
+        startIndex = 0;
+        endIndex = maxLength;
+        System.out.println("ERRMSG1");
+
+      }
+      String substring = data.substring(startIndex, endIndex);
+      return substring;
+
+    }
+    return data;
   }
 }
