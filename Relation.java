8dd02cd6bec489b197b3b534db824b18afbd4aeb
Stephen Hyde-Donohue <Stephen@wlan010-111-084-196.wlan.wwu.edu>
2017-11-27 13:06:05 -0800

    Quick change

--- Relation.java
+++ Relation.java
@@ -51,6 +51,18 @@ public class Relation {
     return maxlen;
   }
 
+  /* Checks qualifications for delete where and select where */
+  public boolean testTuple(String tuple, String qualification) {
+    /* Identify AND groups */
+    String[] andGroups = qualifications.trim().split("\\sand\\s,\\s|;|\\s");
+    for () {
+
+    }
+    /* check for bounds */
+    int upperBound;
+    int lowerBound;
+  }
+
   public void print() {
     String maxlen;
     int tuplesLen = tuples.size();
